cf7373b32685a76392d1ba445dbd80567a5bd732
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2014-09-08 09:24:54 -0500

    fix LWP feed

--- corn.pl
+++ corn.pl
@@ -7,27 +7,19 @@ use autodie;
 use experimental 'signatures';
 
 use Feed::Pipe;
-use IO::Async::Loop;
-use Net::Async::HTTP;
-use URI;
 use XML::Feed;
-use DateTime::Format::Strptime;
-use Try::Tiny;
-
-my $loop = IO::Async::Loop->new;
-my $http = Net::Async::HTTP->new;
-$loop->add( $http );
+use Future;
+use LWP::UserAgent;
 
 sub feed ($url, $name, $commit = 0, $transform = sub { $_[0] }) {
-   $http->do_request(
-      uri => URI->new( $url ),
-   )->then(sub ($res) {
+   _do_req($url)
+   ->then(sub ($res) {
       my %seen;
 
       my $path = "./$name.xml";
 
       my $f = Feed::Pipe->new
-         ->cat(\($res->content), ( -f $path ? ($path) : ()))
+         ->cat(\($res->decoded_content), ( -f $path ? ($path) : ()))
          ->grep(sub { !($seen{$_->id}++) })
          ->grep(sub { $_->issued gt '' . DateTime->now->subtract(days => 30) });
       my $a = $f->as_atom_obj;
@@ -49,48 +41,29 @@ my $f = feed(
    'LWN',
    $commit,
    sub ($s) {
-
-      $s
-         ->map(sub {
-            if ($_->title =~ m/\[\$\]/) {
-               my $ready_date = _forward_to_thu(_parse_dt($_->issued));
-               $_->title($_->title =~ s/\[\$\]//)
-                  if DateTime->now > $ready_date && $http->do_request( uri => URI->new( $_->link->href ) )
-                  ->then(sub ($res) {
-                     if ($res->content !~ 'this item will become freely available on ' . $ready_date->strftime('%B %d, %Y')) {
-                        warn " !!! WTF $ready_date is different on the page!";
-                        return Future->done(0)
-                     }
-                  })->get;
-
-            }
-            return $_
-         })
-         ->grep(sub { $_->title !~ m/\[\$\]/ });
+      $s->map(sub {
+         if ($_->title =~ m/\[\$\]/) {
+            $_->title($_->title =~ s/\[\$\]//)
+               if _do_req($_->link->href )
+                  ->then_done(1)
+                  ->else_done(0)
+                  ->get;
+         }
+         return $_
+      })->grep(sub { $_->title !~ m/\[\$\]/ });
    },
 )->get;
 
 say $f->as_xml;
 
-sub _forward_to_thu ($dt) {
-   my $ret = $dt->clone;
+sub _do_req ($url) {
+   my $ua = LWP::UserAgent->new;
+   $ua->timeout(10);
+   my $response = $ua->get($url);
 
-   while ($ret->day_name ne 'Thursday') {
-      $ret = $ret->add(days => 1)
-   }
-
-   return $ret
-}
-
-sub _parse_dt ($str) {
-   try {
-      DateTime::Format::Strptime->new(
-         pattern   => '%FT%TZ',
-         locale    => 'en_US',
-         time_zone => 'UTC',
-         on_error  => 'croak',
-      )->parse_datetime($str)
-   } catch {
-      die "$_: $str"
+   if ($response->is_success) {
+      return Future->done($response)
+   } else {
+      return Future->fail($response)
    }
 }
